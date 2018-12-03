3e72d296d37fc38c53dd4ab60aa1eca33afed07b
Delshad <delshadk1@gmail.com>
2018-12-03 13:41:27 +0100

    Gameboard  ex layout

--- www/js/GamePage.js
+++ www/js/GamePage.js
@@ -15,7 +15,7 @@ class GamePage extends Component {
     let gameBox = `<div class="game-box"></div>`; //7
     let horiRow = `<tr class="hori-row"></tr>`   //6
     let count = 0;
-    let count2 = 0;
+    let boxClass = 0;
     let firstRow = 0;
 
     $(function () {
@@ -23,10 +23,12 @@ class GamePage extends Component {
       for (let i = 0; i < 42; i++) {
 
 
-        if (count <= 42 && count2 === 0) {
+        if (count <= 42 ) {
 
-          $('.row').append(gameBox);
+          $('.row').append(gameBox).addClass(boxClass);
+          
           count++
+          boxClass++;
 
         }
 
