f82e7a5c222a1ce555f98c5ab1084fbc8678897a
Kim <kim.johnsson@telia.com>
2018-12-03 11:21:30 +0100

    navbar links

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -4,7 +4,9 @@ class NavBar extends Component {
     super();
     this.navItems = [
       new NavItem('Start', '/'),
-      new NavItem('About', '/about')
+      new NavItem('Spela', '/spela'),
+      new NavItem('Spelregler', '/spelregler'),
+      new NavItem('Hiscore', '/hiscore')
     ];
   }
 
