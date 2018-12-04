e5aa3a75416d6f253d7b7a0d82e1e2441a065ee3
Fanny Andersson <fanny_andersson@hotmail.com>
2018-12-04 10:17:42 +0100

    Deleted aboutpage

--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -3,7 +3,6 @@ class PageContent extends Component {
   constructor() {
     super();
     this.startPage = new StartPage();
-    this.aboutPage = new AboutPage();
     this.gamestartPage = new GameStartPage();
     this.missingPage = new MissingPage();
     this.gameBoard = new GameBoard();
