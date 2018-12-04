b6fcfdf8df44257a629e85b46a7328dcb0707544
David <David_stralfors@hotmail.com>
2018-12-04 17:42:18 +0100

    More win condition

--- www/js/GameBoard.js
+++ www/js/GameBoard.js
@@ -39,7 +39,7 @@ class GameBoard extends Component {
       for (col = 0; col < 4; col++) {
         for (row = 0; row < 6; row++) {          
           if (this.slots[row][col].value == i) {
-            if ((this.slots[row][col+1].value == i) && (this.slots[row][col+2].value == i) && (this.slots[row][col+3].value == i)) {
+            if ((this.slots[row][col + 1].value == i) && (this.slots[row][col + 2].value == i) && (this.slots[row][col + 3].value == i)) {
               gameOver(i);
               return true;
             }
@@ -53,7 +53,21 @@ class GameBoard extends Component {
       for (col = 0; col < 7; col++) {
         for (row = 0; row < 3; row++) {          
           if (this.slots[row][col].value == i) {
-            if ((this.slots[row+1][col].value == i) && (this.slots[row+2][col].value == i) && (this.slots[row+3][col].value == i)) {
+            if ((this.slots[row + 1][col].value == i) && (this.slots[row + 2][col].value == i) && (this.slots[row + 3][col].value == i)) {
+              gameOver(i);
+              return true;
+            }
+          }
+        }
+      }
+    }
+
+    //diagonally down
+    for (i = 1; i < 3; i++) {      
+      for (col = 0; col < 4; col++) {        
+        for (row = 0; row < 3; row++) {          
+          if (this.slots[row][col].value == i) {
+            if ((this.slots[row + 1][col + 1].value == i) && (this.slots[row + 2][col + 2].value == i) && (this.slots[row + 3][col + 3].value == i)) {
               gameOver(i);
               return true;
             }
