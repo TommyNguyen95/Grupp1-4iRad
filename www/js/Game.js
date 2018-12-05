12f92d759420e26756ecd9bec56c5e4491afbd07
Fanny Andersson <fanny_andersson@hotmail.com>
2018-12-05 22:23:04 +0100

    Try to fix length of the name

--- www/js/Game.js
+++ www/js/Game.js
@@ -1,8 +1,9 @@
 class Game extends Component {
 
-    constructor(players) {
+    constructor(players, type) {
         super();
         this.players = players;
+        this.type = type;
         this.gameBoard = new GameBoard();
         console.log(this);
     }
--- www/js/GameStartPage.js
+++ www/js/GameStartPage.js
@@ -8,11 +8,24 @@ class GameStartPage extends Component {
         })
     }
 
-    // create a method thats create objects
+
+
     startGame() {
         let p1 = new Player(this.baseEl.find('#name1').val());
         let p2 = new Player(this.baseEl.find('#name2').val());
         this.game = new Game([p1, p2]);
+        let nrOfLetters = 0;
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        for (let i = 0; p1.length && p2.length; i++)
+            if (letters.includes(p1[i]) && p2[i]) {
+                nrOfLetters++;
+                console.log('Fanny');
+            } else {
+                break;
+            }
+        if (nrOfLetters < 2 && nrOfLetters > 10) {
+            console.log('No');
+        }
     }
-
 }
+
