b9ad5da4d2108478836129b4dfbd2cf34821f151
Fanny Andersson <fanny_andersson@hotmail.com>
2018-12-06 12:25:09 +0100

    Deleted if statement

--- www/js/GameStartPage.js
+++ www/js/GameStartPage.js
@@ -14,18 +14,6 @@ class GameStartPage extends Component {
         let p1 = new Player(this.baseEl.find('#name1').val());
         let p2 = new Player(this.baseEl.find('#name2').val());
         this.game = new Game([p1, p2]);
-        let nrOfLetters = 0;
-        let letters = 'abcdefghijklmnopqrstuvwxyz';
-        for (let i = 0; p1.length && p2.length; i++)
-            if (letters.includes(p1[i]) && p2[i]) {
-                nrOfLetters++;
-                console.log('Fanny');
-            } else {
-                break;
-            }
-        if (nrOfLetters < 2 && nrOfLetters > 10) {
-            console.log('No');
-        }
+
     }
 }
-
