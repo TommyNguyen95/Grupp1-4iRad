281864223217d811283096abb1f447c020104b87
Sudha Parimala <mailme.sudha.k@gmail.com>
2018-12-06 12:28:47 +0100

    Game Rules page details

---
+++ www/js/GameRulesPage.js
@@ -0,0 +1,1 @@
+class GameRulesPage extends Component {}
--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -6,6 +6,7 @@ class PageContent extends Component {
     this.gamestartPage = new GameStartPage();
     this.missingPage = new MissingPage();
     this.gameBoard = new GameBoard();
+    this.gameRulesPage = new GameRulesPage();
     this.hiscorePage = new HiscorePage();
     this.davGame = new DavGame();
   }
