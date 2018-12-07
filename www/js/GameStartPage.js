21efbdbfb76dde5334b361980fcd2e5738f389e7
David <David_stralfors@hotmail.com>
2018-12-07 21:22:23 +0100

    reset GameStartPage

--- www/js/GameStartPage.js
+++ www/js/GameStartPage.js
@@ -12,34 +12,10 @@ class GameStartPage extends Component {
 
 
     startGame() {
-        if(this.baseEl.find('#type1').val() === 'Computer'){
-            this.baseEl.find('#name1').val('Computer1');
-        }
-        if(this.baseEl.find('#type2').val() === 'Computer'){
-            this.baseEl.find('#name2').val('Computer2');
-        }       
-        try {
-            if(!this.baseEl.find('#name1').val().match(/^[A-Za-z1-2]+$/) || this.baseEl.find('#name1').val().length < 2 || this.baseEl.find('#name1').val().length > 10
-             || !this.baseEl.find('#name2').val().match(/^[A-Za-z1-2]+$/) || this.baseEl.find('#name1').val().length < 2 || this.baseEl.find('#name1').val().length > 10){ 
-            throw 'Names have to be 2 to 10 letters long';
-            }            
-        }
-        catch(err) {
-            alert(err);
-            return;
-        }
-        try {
-            if(this.baseEl.find('#type1').val() === 'Choose type...' || this.baseEl.find('#type2').val() === 'Choose type...'){
-                throw 'You must choose types'; 
-            }
-        }
-        catch(err){
-            alert(err);
-            return;
-        }
-        this.player1 = (this.baseEl.find('#type1').val() === 'Human') ? (new Player(this.baseEl.find('#name1').val(), 1)) : (new Bot(1));
-        this.player2 = (this.baseEl.find('#type2').val() === 'Human') ? (new Player(this.baseEl.find('#name2').val(), 2)) : (new Bot(2));        
-        this.game = new Game(this.player1, this.player2);                   
+        let p1 = new Player(this.baseEl.find('#name1').val());
+        let p2 = new Player(this.baseEl.find('#name2').val());
+        this.game = new Game([p1, p2]);
+        console.log(this.game);
         this.render();
     }
     // when I quit the game this method will bring back the form
