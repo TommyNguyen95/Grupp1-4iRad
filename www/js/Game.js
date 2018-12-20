cf7d02db95f2371458f2e547ee03b0cba3ed94b5
Delshad <delshadk1@gmail.com>
2018-12-20 13:47:55 +0100

    Winning line
    Code fix

--- www/js/Game.js
+++ www/js/Game.js
@@ -67,7 +67,7 @@ class Game extends Component {
   async fall(col) {
     if (this.doIgnore) {
       return;
-    }    
+    }
     for (let row = 0; row < 6; row++) {
       if (this.board[row][col].value == 0) {
         if (this.activePlayer === 1) {
@@ -282,9 +282,6 @@ class Game extends Component {
     this.winCount = (player === 1) ? this.moveCounter1 : this.moveCounter2;
     this.winner = (player === 1) ? this.player1.name : this.player2.name;
     this.render();
-    App.highscorePage.addHiScore(this.winner, this.winCount);
-    setTimeout(function(){$('.win-modal').modal('show');}, 100);
-    
     if (this.winner == 'Computer1' || this.winner == 'Computer2') {
     } else {
       App.highscorePage.addHiScore(this.winner, this.winCount);
